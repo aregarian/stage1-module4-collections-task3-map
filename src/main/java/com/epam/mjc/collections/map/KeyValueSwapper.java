8cffafe2dc51310f11e5479b8bd85d6ae043d8b5
Fanny Dorthey <fannydor@un-k4.kzn.21-school.ru>
2023-11-09 17:49:55 +0300

    asdadasdasdasdasdasdasd

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -5,7 +5,37 @@ import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
         Map<String,Integer> hehe = new HashMap<>();
+
+        for (Map.Entry<Integer,String> me :
+                sourceMap.entrySet()) {
+
+            if (sourceMap.containsValue(me.getValue())) {
+                sourceMap.
+            }
+
+        }
+
+
+
+
+
+
+
+        for (Map.Entry<Integer,String> me :
+                sourceMap.entrySet()) {
+
+
+            hehe.put(me.getValue(),me.getKey());
+
+        }
+
+
+
+
+
+
         return hehe;
     }
 }

b2ce0f03dab07f6381227e8ca766210356d294b9
Fanny Dorthey <fannydor@un-k4.kzn.21-school.ru>
2023-11-09 16:52:22 +0300

    asdadasd

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        return true;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer,Integer> hehe = new HashMap<>();
+        return hehe;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> hehe = new HashMap<>();
+        sentence = sentence.toLowerCase();
+        StringTokenizer str = new StringTokenizer(sentence, " .,");
+        Map<String, Integer> hehe1 = new HashMap<>();
+
+        int i = 0;
+        while (str.hasMoreTokens()) {
+            String sss = str.nextToken();
+            if (hehe.containsKey(sss)) {
+                hehe.put(sss, 2);
+            }else {
+                hehe.put(sss, 1 + i);
+            }
+
+
+
+        }
+        return hehe;
+
     }
 }
